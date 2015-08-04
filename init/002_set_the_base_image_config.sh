2fc6d08fb2e5a2dbe7b619cfebdfe84723b9f4e9
Stian Buch Larsen <lonixx@gmail.com>
2015-08-04 23:15:02 +0200

    Copy confs from correct locations

--- init/002_set_the_base_image_config.sh
+++ init/002_set_the_base_image_config.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 mkdir -p /config/nginx/site-confs /config/www
 if [ ! -f "/config/nginx/nginx.conf" ]; then
-cp /root/config-files/nginx.conf /config/nginx/nginx.conf
+cp /defaults/nginx.conf /config/nginx/nginx.conf
 fi
 
 if [ ! -f "/config/nginx/nginx-fpm.conf" ] ; then
-cp /root/config-files/nginx-fpm.conf /config/nginx/nginx-fpm.conf
+cp /defaults/nginx-fpm.conf /config/nginx/nginx-fpm.conf
 fi
 /bin/cp -f /config/nginx/nginx-fpm.conf /etc/php5/fpm/pool.d/www.conf
 chown -R abc:abc /config
